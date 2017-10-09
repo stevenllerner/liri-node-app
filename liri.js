91c30ed05550e5caeac253e0e58331be97cf7dfd
Steven L Lerner <stevenllerner@gmail.com>
2017-10-08 21:05:06 -0500

    twitter functionality complete

--- liri.js
+++ liri.js
@@ -14,29 +14,35 @@ var client = new Twitter({
   access_token_key: twitterKeys.access_token_key,
   access_token_secret: twitterKeys.access_token_secret
 });
-
-// var params = {screen_name: 'samxmoses'}
-// client.get("statuses/user_timeline", params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log(tweets);  // The favorites.      
-//   } else {
-//     console.log(error)
-//   }
-// });
-
+//Recalls alias' Sam X Moses' last two tweets
+function tweetRecall(){
+	var params = {screen_name: 'samxmoses'}
+	var i = 2;
+	client.get("statuses/user_timeline", params, function(error, tweets, response) {
+	  if (!error) {
+	    for (i=0; i<2; i++){
+	    	j=i+1;
+	    	console.log("Sam X Moses tweet # "+j+": "+tweets[i].text);	    	
+	    }  
+	  } else {
+	    console.log(error)
+	  }
+	});
+};
 
 var Spotify = require('node-spotify-api');
- 
 var spotify = new Spotify({
   id: "381cb14110d54d0f928c1b9e098443af",
   secret: "8240a513ced1422aacf3c12d75c568da"
 });
  
-spotify.search({ type: "track", query: 'The+Sign', limit: 2 }, function(err, data) {
+spotify.search({ type: "track", query: 'The Sign Ace of Base', limit: 1 }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
- 
-console.log(JSON.stringify(data, undefined, 2)); 
+ 	console.log(JSON.stringify(data, undefined, 2));
+ 	console.log("Artist: "+JSON.stringify(data.tracks.items[0].artists[0].name));
+ 	console.log("Song's Name: "+)
 });
 
+//tweetRecall();
