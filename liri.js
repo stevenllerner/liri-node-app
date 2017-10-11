c4fd571ff926174303bfa536a2008e2902dc64c0
Steven L Lerner <stevenllerner@gmail.com>
2017-10-10 19:38:25 -0500

    twitter, spotify, imdb and fs all working

--- liri.js
+++ liri.js
@@ -1,19 +1,19 @@
+
+
+var request = require("request");
 var twitterKeys = require("./keys.js");
-
-//console.log(twitterKeys);
-
-var input = process.argv;
-
-//console.log (input);
-
 var Twitter = require('twitter');
- 
+var fs = require("fs");
+
 var client = new Twitter({
   consumer_key: twitterKeys.consumer_key,
   consumer_secret: twitterKeys.consumer_secret,
   access_token_key: twitterKeys.access_token_key,
   access_token_secret: twitterKeys.access_token_secret
 });
+
+
+
 //Recalls alias' Sam X Moses' last two tweets
 function tweetRecall(){
 	var params = {screen_name: 'samxmoses'}
@@ -29,46 +29,55 @@ function tweetRecall(){
 	  }
 	});
 };
+
 function spotifyRecall(){
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify({
-  id: "381cb14110d54d0f928c1b9e098443af",
-  secret: "8240a513ced1422aacf3c12d75c568da"
-});
- 
-spotify.search({ type: "track", query: 'The Sign Ace of Base', limit: 1 }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-// 	console.log(JSON.stringify(data, undefined, 2));
- 	console.log("Artist: "+JSON.stringify(data.tracks.items[0].artists[0].name));
- 	console.log("Song's Name: "+ JSON.stringify(data.tracks.items[0].name));
- 	console.log("Preview on: "+ data.tracks.items[0].external_urls.spotify);
-});
+  var Spotify = require('node-spotify-api');
+  var spotify = new Spotify({
+    id: "381cb14110d54d0f928c1b9e098443af",
+    secret: "8240a513ced1422aacf3c12d75c568da"
+  });
+   
+  spotify.search({ type: "track", query: 'The Sign Ace of Base', limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   	console.log("Artist: "+JSON.stringify(data.tracks.items[0].artists[0].name));
+   	console.log("Song's Name: "+ JSON.stringify(data.tracks.items[0].name));
+   	console.log("Preview on: "+ data.tracks.items[0].external_urls.spotify);
+  });
 };
 
+function imdbRecall(){
+  var queryURL = "http://www.omdbapi.com/?apikey=40e9cece&t=" + "Mr. Nobody" + "&y=&plot=short";
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-<script type="text/javascript">
+  request(queryURL, function (error, response, body) {
+    if (error){
+        return console.log("Error occurred: " + error);
+      } else {
+    var y = JSON.parse(body);
+    console.log("Title of the movie: "+y.Title);
+    console.log("Year movie came out: "+y.Year);
+    console.log("IMDB rating: "+y.imdbRating);
+    console.log("Rotten Tomatoes rating: "+y.Ratings[1].Value);
+    console.log("Country where movie was produced: "+y.Country);
+    console.log("Language of the movie: "+y.Language);
+    console.log("Movie plot: "+y.Plot);
+    console.log("Actors: "+y.Actors);
+    };
+  });
+};
+
+fs.readFile("./random.txt", "utf8", function (err, data){
+  if (err){
+    return console.log("Error occurred: " + err);
+  } else {
+    console.log(data);
+  }
 
 
-var queryURL = "http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=40e9cece";
+})
 
-        // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-        // and display it in the div with an id of movie-view
-
-        //------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
-
-$.ajax({
-	url: queryURL,
-    method: "GET"
-}).done(function(response) {
-    console.log(JSON.stringify(response));
-//  $("#movie-view").text(JSON.stringify(response));
-});
-
-
-
+var input = process.argv;
 
 
 
