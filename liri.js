2ecea8956b28ea6377500fc67d453481e96a08e5
Steven L Lerner <stevenllerner@gmail.com>
2017-10-10 21:03:17 -0500

    completed project

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
 
-
 var request = require("request");
 var twitterKeys = require("./keys.js");
 var Twitter = require('twitter');
@@ -12,8 +11,6 @@ var client = new Twitter({
   access_token_secret: twitterKeys.access_token_secret
 });
 
-
-
 //Recalls alias' Sam X Moses' last two tweets
 function tweetRecall(){
 	var params = {screen_name: 'samxmoses'}
@@ -22,7 +19,7 @@ function tweetRecall(){
 	  if (!error) {
 	    for (i=0; i<2; i++){
 	    	j=i+1;
-	    	console.log("Sam X Moses tweet # "+j+": "+tweets[i].text);	    	
+	    	console.log("Alias Sam X Moses tweet # "+j+": "+tweets[i].text);	    	
 	    }  
 	  } else {
 	    console.log(error)
@@ -30,14 +27,14 @@ function tweetRecall(){
 	});
 };
 
-function spotifyRecall(){
+function spotifyRecall(songName){
   var Spotify = require('node-spotify-api');
   var spotify = new Spotify({
     id: "381cb14110d54d0f928c1b9e098443af",
     secret: "8240a513ced1422aacf3c12d75c568da"
   });
    
-  spotify.search({ type: "track", query: 'The Sign Ace of Base', limit: 1 }, function(err, data) {
+  spotify.search({ type: "track", query: songName, limit: 1 }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
@@ -47,18 +44,19 @@ function spotifyRecall(){
   });
 };
 
-function imdbRecall(){
-  var queryURL = "http://www.omdbapi.com/?apikey=40e9cece&t=" + "Mr. Nobody" + "&y=&plot=short";
+function imdbRecall(movieName){
+  var queryURL = "http://www.omdbapi.com/?apikey=40e9cece&t=" + movieName + "&y=&plot=short";
 
   request(queryURL, function (error, response, body) {
     if (error){
         return console.log("Error occurred: " + error);
       } else {
     var y = JSON.parse(body);
+//    console.log(y);
     console.log("Title of the movie: "+y.Title);
     console.log("Year movie came out: "+y.Year);
     console.log("IMDB rating: "+y.imdbRating);
-    console.log("Rotten Tomatoes rating: "+y.Ratings[1].Value);
+    console.log("Rotten Tomatoes rating: "+y.Ratings[0].Value);
     console.log("Country where movie was produced: "+y.Country);
     console.log("Language of the movie: "+y.Language);
     console.log("Movie plot: "+y.Plot);
@@ -66,36 +64,42 @@ function imdbRecall(){
     };
   });
 };
-
-fs.readFile("./random.txt", "utf8", function (err, data){
-  if (err){
-    return console.log("Error occurred: " + err);
-  } else {
-    console.log(data);
-  }
-
-
-})
+var randomInput = [];
+function randTextInst(){
+  fs.readFile("./random.txt", "utf8", function (err, data){
+    if (err){
+      return console.log("Error occurred: " + err);
+    } else {
+      randomInput = data.split(",");
+      input[2] = randomInput[0];
+      input[3] = randomInput[1];
+      if (input[2] === "my-tweets") {
+        tweetRecall();
+      };
+      if (input[2] === "spotify-this-song") {
+        spotifyRecall(input[3]);
+      };
+      if (input[2] === "movie-this") {
+      imdbRecall(input[3]);
+      };
+    };
+  });
+};
 
 var input = process.argv;
+//console.log(input);
+if (input[2] === "do-what-it-says") {
+  randTextInst();
+} else {
+  if (input[2] === "my-tweets") {
+    tweetRecall();
+  };
+  if (input[2] === "spotify-this-song") {
+    spotifyRecall(input[3]);
+  };
+  if (input[2] === "movie-this") {
+    imdbRecall(input[3]);
+  };
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// tweetRecall();
-// spotifyRecall();
 
